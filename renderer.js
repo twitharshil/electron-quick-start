496a2dc841b6668fc5370fd953217fdfdbebf0b7
Harshil Jain <twitharshil@gmail.com>
2021-12-06 15:28:48 +0530

    exmaples application to try out interactivity

--- renderer.js
+++ renderer.js
@@ -4,3 +4,33 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const button = document.getElementById('button-id');
+button.addEventListener('click', () => {
+  createBrowserWindow();
+});
+
+function createBrowserWindow() {
+  const { shell } = require('electron')
+  shell.openExternal('https://www.postman.com')
+}
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
